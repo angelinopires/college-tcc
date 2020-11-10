24653f66303a122c9e306ec17610051892b41559
Angelino Oliveira <lino.pires@live.com>
2020-11-10 17:25:26 -0300

    feat: add login module

---
+++ src/app/modules/authentication/authentication.module.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+// COMPONENTS
+import { LoginComponent } from './pages/login/login.component';
+
+// MODULES
+import { AuthenticationRoutingModule } from './authentication-routing.module'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialUiModule } from '@material-ui/material-ui.module';
+
+// SERVICE
+import { LoginService, StorageService } from '@services';
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    AuthenticationRoutingModule,
+    CommonModule,
+    FormsModule,
+    MaterialUiModule,
+    ReactiveFormsModule,
+    RouterModule
+  ],
+  providers: [
+    LoginService,
+    StorageService
+  ]
+})
+export class AuthenticationModule { }
