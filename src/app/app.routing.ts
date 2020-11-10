ef905af0f073da806ae57bb9b3732329783d2bd1
Angelino Oliveira <lino.pires@live.com>
2020-11-10 17:25:26 -0300

    feat: add login module

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,7 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: './modules/home/home.module#HomeModule'
+    loadChildren: './modules/authentication/authentication.module#AuthenticationModule'
   },
   {
     path: 'cotacoes',
---
+++ src/app/modules/authentication/pages/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { LoginService } from '@services';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor (
+    private _formBuilder: FormBuilder,
+    private _loginService: LoginService
+    ) {
+      this.loginForm = this._formBuilder.group({
+        email: ['', Validators.required],
+        password: ['', Validators.required]
+      })
+    }
+
+  onLoginSubmit(): void {
+    if (this.loginForm.valid && this.loginForm.dirty) {
+      const { email, password} = this.loginForm.value
+
+      this._loginService.login(email, password)
+    }
+  }
+}
