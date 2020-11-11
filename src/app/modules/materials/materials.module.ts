fd70de3803639345979901b5643622c1156cd04e
Angelino Oliveira <lino.pires@live.com>
2020-11-11 15:51:31 -0300

    feat: add materials dashboard page

--- src/app/modules/materials/materials.module.ts
+++ src/app/modules/materials/materials.module.ts
@@ -7,14 +7,16 @@ import { DashboardComponent } from './pages';
 
 // MODULES
 import { MaterialsRoutingModule } from './materials-routing.module';
+import { MaterialUiModule } from '@material-ui/material-ui.module';
 import { SharedModule } from '@shared/shared.module';
 
 // SERVICES
 import { ApiService } from '@services/api/api.service';
+import { MaterialService } from '@services/material/material.service';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, MaterialsRoutingModule, RouterModule, SharedModule],
-  providers: [ApiService]
+  imports: [CommonModule, MaterialsRoutingModule, RouterModule, SharedModule, MaterialUiModule],
+  providers: [ApiService, MaterialService]
 })
 export class MaterialsModule {}
--- src/app/modules/materials/pages/dashboard/dashboard.component.ts
+++ src/app/modules/materials/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MaterialService } from '@services/material/material.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
-  constructor() {}
+export class DashboardComponent implements OnInit, AfterViewInit {
+  dataSource = new MatTableDataSource();
+  columnsToDisplay = ['id', 'description', 'unity', 'group'];
 
-  ngOnInit(): void {}
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor (private _materialService: MaterialService) {}
+
+  ngOnInit (): void {
+    this.dataSource.data = this._materialService.getMaterials()
+  }
+
+  ngAfterViewInit (): void {
+    this.dataSource.sort = this.sort
+  }
+
+  applyFilter (filterValue: string): void {
+    this.dataSource.filter = filterValue.trim().toLocaleLowerCase()
+  }
 }
